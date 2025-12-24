98b7249ef51b8e8191d18a5f0a24695141c9bf44
Jameson Papin <jamesonpapin@yahoo.com>
2025-12-24 12:30:53 -0800

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@ async fn health_check() -> HttpResponse {
 }
 
 pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+        })
         .bind("127.0.0.1:8000")?
         .run()
         .await
